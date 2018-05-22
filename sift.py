09de1c6e87eb2c2f9ab1b27cbc704f6fb5d931ad
Siegel16 <truongpm@haposoft.com>
2018-05-22 16:49:13 +0700

    tiep

--- sift.py
+++ sift.py
@@ -4,13 +4,13 @@ import os
 from os import listdir
 from os.path import isfile, join
 
+fileName = []
 sift = cv2.xfeatures2d.SIFT_create()
 path = 'training'
 files = [ f for f in listdir(path) if isfile(join(path,f)) ]
 images = np.empty(len(files), dtype=object)
 dirFolder = 'sift'
 os.mkdir(dirFolder)
-fileName = []
 for n in range(0, len(files)):
   images[n] = cv2.imread( join(path,files[n]))
   gray = cv2.cvtColor(images[n],cv2.COLOR_BGR2GRAY)
--- test.py
+++ test.py
@@ -6,19 +6,35 @@ import glob
 trainingData = []
 testData = []
 
-# def getKpForTestData(img,file):
-#     sift = cv2.xfeatures2d.SIFT_create()
-#     image = cv2.imread(img + '.jpg',1)
-#     gray= cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-#     kp = sift.detect(image,None)
-#     kp,des = sift.compute(gray,kp)
-#     np.savetxt(file + '.txt' ,des,delimiter=',')
-# getKpForTestData('test2','test')
+# img = cv2.imread('training/i037sb-fn.jpg',0)
+# cv2.imshow('test', img)
+# cv2.waitKey(0)
+
+def getKpForTestData(img,file):
+    sift = cv2.xfeatures2d.SIFT_create()
+    image = cv2.imread(img + '.jpg',1)
+    gray= cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+    kp = sift.detect(image,None)
+    kp,des = sift.compute(gray,kp)
+    np.savetxt(file + '.txt' ,des,delimiter=',')
+getKpForTestData('test2','test')
 
 def euclidean(vector1, vector2):
     dist = [(a - b)**2 for a, b in zip(np.array(vector1), np.array(vector2))]
     dist = math.sqrt(sum(dist))
     return dist
+
+def findDistance(v1, v2, t):
+    count = 0
+    for i in v1:
+        distance = []
+        for j in v2:
+            dist = euclidean(i,j)
+            # distance.append(dist)
+        # minDistance = min(float(s) for s in distance)
+            if (dist < t):
+                count +=1
+    return float(count) / float(len(v1) * len(v2))
 
 def readTestData(name) :
     name = name + '.txt'
@@ -33,20 +49,22 @@ def readTrainingData(p):
     files = glob.glob(path)
     for name in files:
         with open(name) as f:
-            data = f.read().strip().replace('\n', ',').split(',')
-            arrayParse = []
+            data = f.read().strip().splitlines()
+            dataArrParse = []
             for value in data:
-                dataParse = float(value)
-                arrayParse.append(dataParse)
-    trainingData.append(arrayParse)
+                arr = value.split(',')
+                arrayParse = []
+                for j in arr:
+                    dataParse = float(j)
+                    arrayParse.append(dataParse)
+                dataArrParse.append(arrayParse)
+            trainingData.append(dataArrParse)
+    arrResult = []
+    for img in trainingData:
+        result = findDistance(testData,img,200)
+        arrResult.append(result)
+    maxResult = max(arrResult)
+    index = arrResult.index(maxResult)
+    print arrResult
+    # [i for i, j in enumerate(arrResult) if j == maxResult]
 readTrainingData('sift')
-print trainingData[0][0]
-def findDistance(v1, v2, t):
-    distance = []
-    for i in v1:
-        for j in v2:
-            dist = euclidean(i,j)
-            distance.append(dist)
-    # minDistance = min(float(s) for s in distance)
-    return minn
-# print(findDistance(testData, trainingData,400))
